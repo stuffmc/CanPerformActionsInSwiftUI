bc5924aec6209dca067c902f8499dc985e99384e
StuFF mc <m+git@carrascomolina.com>
2021-11-04 19:25:05 +0100

    Initial Commit

---
+++ CanPerformActionsInSwiftUI/CanPerformActionsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanPerformActionsInSwiftUIApp.swift
+//  CanPerformActionsInSwiftUI
+//
+//  Created by StuFF mc on 04.11.21.
+//
+
+import SwiftUI
+
+@main
+struct CanPerformActionsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanPerformActionsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CanPerformActionsInSwiftUI
+//
+//  Created by StuFF mc on 04.11.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
